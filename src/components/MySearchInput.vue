3cc780301682cc96ae07d808406e2218c6ca4925
Tony Trinh <tony19@gmail.com>
2021-11-01 20:06:12 -0500

    add MySearchInput

---
+++ src/components/MySearchInput.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import HelloWorld from './HelloWorld.vue'
+
+const searchRef = ref<HTMLElement>()
+onMounted(() => { searchRef.value?.focus() })
+
+const helloRef = ref<InstanceType<typeof HelloWorld>>()
+onMounted(() => { helloRef.value?.msg })
+</script>
+
+<template>
+  <input type="search" ref="searchRef">
+  <HelloWorld ref="helloRef" msg="hello world" />
+</template>
